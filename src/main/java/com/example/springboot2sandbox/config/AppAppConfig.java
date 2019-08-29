2b9778c29316e5df8b0404c0879bbca1cf174c83
Ryosuke Uchitate <ruchitate@coiney.com>
2019-08-29 18:54:52 +0900

    Beanメソッドで `@ConfigurationProperties` 使う場合

--- src/main/java/com/example/springboot2sandbox/config/AppAppConfig.java
+++ src/main/java/com/example/springboot2sandbox/config/AppAppConfig.java
@@ -2,11 +2,14 @@ package com.example.springboot2sandbox.config;
 
 import com.example.springboot2sandbox.properties.*;
 import org.springframework.boot.context.properties.*;
-import org.springframework.boot.test.context.*;
 import org.springframework.context.annotation.*;
 
-@TestConfiguration
-@Import(AppAppConfig.class)
-@EnableConfigurationProperties(value = AppProperties.class)
-public class TestConfig {
+@Configuration
+public class AppAppConfig {
+
+	@Bean
+	@ConfigurationProperties("app.app")
+	public AppAppProperties appAppProperties() {
+		return new AppAppProperties();
+	}
 }
--- src/test/java/com/example/springboot2sandbox/ConfigurationPropertyTest.java
+++ src/test/java/com/example/springboot2sandbox/ConfigurationPropertyTest.java
@@ -11,15 +11,20 @@ import org.springframework.test.context.*;
 import org.springframework.test.context.junit.jupiter.*;
 
 @ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "/app-test.properties", properties = "spring.config.name=app")
 @ContextConfiguration(classes = {TestConfig.class}, initializers = ConfigFileApplicationContextInitializer.class)
 class ConfigurationPropertyTest {
 
 	@Autowired
 	private AppProperties appProperties;
+	@Autowired
+	private AppAppProperties appAppProperties;
 
 	@Test
 	void getValue() {
 		Assertions.assertThat(appProperties.getDomain())
 				.isEqualTo("b1a9idps.com");
+		Assertions.assertThat(appAppProperties.getName())
+				.isEqualTo("uchitate");
 	}
 }
